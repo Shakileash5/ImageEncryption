263e263c99818f382ee5cf86fe1e258ff7be9351
shakileash <shakileash2000@gmail.com>
2021-09-27 14:35:24 +0530

    chaos maps algorithms added

--- chaosMap.py
+++ chaosMap.py
@@ -1,8 +1,9 @@
 import cv2
-from pil import Image
+from PIL import Image
 import numpy as np
 import os   
 import random
+from math import log
 
 
 def ArnoldCatTransform(img, num):
@@ -14,15 +15,12 @@ def ArnoldCatTransform(img, num):
             img_arnold[x][y] = img[(x+y)%n][(x+2*y)%n]  
     return img_arnold  
 
-def ArnoldCatEncryption(imageName, key):
-    img = cv2.imread(imageName)
+def ArnoldCatEncryption(img, key):
     for i in range (0,key):
         img = ArnoldCatTransform(img, i)
-    cv2.imwrite(imageName.split('.')[0] + "_ArnoldcatEnc.png", img)
     return img
 
-def ArnoldCatDecryption(imageName, key):
-    img = cv2.imread(imageName)
+def ArnoldCatDecryption(img, key):
     rows, cols, ch = img.shape
     dimension = rows
     decrypt_it = dimension
@@ -36,5 +34,155 @@ def ArnoldCatDecryption(imageName, key):
         decrypt_it = int(12*dimension/7)
     for i in range(key,decrypt_it):
         img = ArnoldCatTransform(img, i)
-    cv2.imwrite(imageName.split('_')[0] + "_ArnoldcatDec.png",img)
     return img
+
+def getImageMatrix(imageName):
+    im = Image.open(imageName) 
+    pix = im.load()
+    color = 1
+    #print(pix)
+    if type(pix[0,0]) == int:
+      color = 0
+    image_size = im.size 
+    image_matrix = []
+    for width in range(int(image_size[0])):
+        row = []
+        for height in range(int(image_size[1])):
+                row.append((pix[width,height]))
+        image_matrix.append(row)
+    return image_matrix,image_size[0],color
+
+def dec(bitSequence):
+    decimal = 0
+    for bit in bitSequence:
+        decimal = decimal * 2 + int(bit)
+    return decimal
+
+def genHenonMap(dimension, key):
+    x = key[0]
+    y = key[1]
+    sequenceSize = dimension * dimension * 8 #Total Number of bitSequence produced
+    bitSequence = []    #Each bitSequence contains 8 bits
+    byteArray = []      #Each byteArray contains m( i.e 512 in this case) bitSequence
+    TImageMatrix = []   #Each TImageMatrix contains m*n byteArray( i.e 512 byteArray in this case)
+    for i in range(sequenceSize):
+        xN = y + 1 - 1.4 * x**2
+        yN = 0.3 * x
+
+        x = xN
+        y = yN
+
+        if xN <= 0.4:
+            bit = 0
+        else:
+            bit = 1
+
+        try:
+            bitSequence.append(bit)
+        except:
+            bitSequence = [bit]
+
+        if i % 8 == 7:
+            decimal = dec(bitSequence)
+            try:
+                byteArray.append(decimal)
+            except:
+                byteArray = [decimal]
+            bitSequence = []
+
+        byteArraySize = dimension*8
+        if i % byteArraySize == byteArraySize-1:
+            try:
+                TImageMatrix.append(byteArray)
+            except:
+                TImageMatrix = [byteArray]
+            byteArray = []
+    return TImageMatrix
+
+def HenonEncryption(imageName,key):
+    imageMatrix, dimension, color = getImageMatrix(imageName)
+    transformationMatrix = genHenonMap(dimension, key)
+    resultantMatrix = []
+    for i in range(dimension):
+        row = []
+        for j in range(dimension):
+            try:
+                if color:
+                    row.append(tuple([transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]]))
+                else:
+                    row.append(transformationMatrix[i][j] ^ imageMatrix[i][j])
+            except:
+                if color:
+                    row = [tuple([transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]])]
+                else :
+                    row = [transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]]
+        try:    
+            resultantMatrix.append(row)
+        except:
+            resultantMatrix = [row]
+    if color:
+      im = Image.new("RGB", (dimension, dimension))
+    else: 
+      im = Image.new("L", (dimension, dimension)) # L is for Black and white pixels
+
+    pix = im.load()
+    for x in range(dimension):
+        for y in range(dimension):
+            pix[x, y] = resultantMatrix[x][y]
+    im.save(imageName.split('.')[0] + "_HenonEnc.png", "PNG")
+    numpy_image=np.array(im)
+    opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR) 
+    #return opencv_image 
+
+def HenonDecryption(imageNameEnc, key):
+    imageMatrix, dimension, color = getImageMatrix(imageNameEnc)
+    transformationMatrix = genHenonMap(dimension, key)
+    henonDecryptedImage = []
+    for i in range(dimension):
+        row = []
+        for j in range(dimension):
+            try:
+                if color:
+                    row.append(tuple([transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]]))
+                else:
+                    row.append(transformationMatrix[i][j] ^ imageMatrix[i][j])
+            except:
+                if color:
+                    row = [tuple([transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]])]
+                else :
+                    row = [transformationMatrix[i][j] ^ x for x in imageMatrix[i][j]]
+        try:
+            henonDecryptedImage.append(row)
+        except:
+            henonDecryptedImage = [row]
+    if color:
+        im = Image.new("RGB", (dimension, dimension))
+    else: 
+        im = Image.new("L", (dimension, dimension)) # L is for Black and white pixels
+
+    pix = im.load()
+    for x in range(dimension):
+        for y in range(dimension):
+            pix[x, y] = henonDecryptedImage[x][y]
+    #im.save(imageNameEnc.split('_')[0] + "_HenonDec.png", "PNG")
+    numpy_image=np.array(im)
+    opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR) 
+    return opencv_image  
+
+
+fileName = "HorizonZero.png"
+img = cv2.imread(fileName)
+print(img.shape)
+if False:
+    img = ArnoldCatEncryption(img, 20)
+    cv2.imwrite("HorizonZero_encrypted.png", img)
+    ArnoldCatDecryptionIm = ArnoldCatDecryption(img, 20)
+    cv2.imwrite("HorizonZero_decrypted.png", ArnoldCatDecryptionIm)
+
+image = "HorizonZero"
+ext = ".png"
+key = (0.1,0.1)
+HenonEncryption(image + ext, key)
+img = HenonDecryption(image + "_HenonEnc.png", key)
+#img = HenonDecryption(img, key)
+cv2.imwrite("HenonDec.png", img)
--- serverCv.py
+++ serverCv.py
@@ -5,36 +5,40 @@ import pickle
 import numpy as np
 import struct ## new
 
-HOST=''
-PORT=8089
+soc = socket.socket() # Create a socket object
+HOST = "127.0.0.1" # server ip
+PORT = 5001 # Reserve a port for server.
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print( 'Socket created')
+soc.bind((HOST, PORT)) # Bind to the port
+soc.listen(5) # Now wait for client connection.
+print(f"[*] Listening as {HOST}:{PORT}")
+# receive 4096 bytes each time
 
-s.bind((HOST,PORT))
-print( 'Socket bind complete')
-s.listen(10)
-print( 'Socket now listening')
+conn, addr = soc.accept() # Establish connection with client.
+print(f"[+] Got a connection from {addr[0]}:{addr[1]}")
 
-conn,addr=s.accept()
-
-### new
 data = b""
 payload_size = struct.calcsize(">L")
 
-print("payload_size: {}".format(payload_size))
+print("[!] payload_size: {}".format(payload_size))
 while len(data) < payload_size:
     print("Recv: {}".format(len(data)))
     data += conn.recv(4096)
-print("Done Recv: {}".format(len(data)))
+
+print("[!] Done Recv: {}".format(len(data)))
 packed_msg_size = data[:payload_size]
 data = data[payload_size:]
 msg_size = struct.unpack(">L", packed_msg_size)[0]
-print("msg_size: {}".format(msg_size))
+print("[!] msg_size: {}".format(msg_size))
+
+
 while len(data) < msg_size:
     data += conn.recv(4096)
+
 frame_data = data[:msg_size]
 data = data[msg_size:]
 frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
 frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 cv2.imwrite('download.jpg',frame)
+
+soc.close()
