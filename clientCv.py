7d5979c8a10c2fd6a2315eb0958f9c453004ea11
shakileash <shakileash2000@gmail.com>
2021-09-27 16:27:02 +0530

    minor changes

--- clientCv.py
+++ clientCv.py
@@ -12,26 +12,24 @@ KEY = "abcdefghijklm"#20#(0.1,0.1)
 
 HOST = "127.0.0.1" # server ip
 PORT = 5001 # Reserve a port for server.
+
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Create a socket object
 sock.connect((HOST,PORT))
+print("[+] Connected to server")
 
-img_counter = 0
+img = cv2.imread('HorizonZero.png') # read image from file
+img = chaosMap.chaosEncryption(img,KEY,TYPE_) # encrypt image
 
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-img = cv2.imread('HorizonZero.png')
-
-img = chaosMap.chaosEncryption(img,KEY,TYPE_)
-#result, img = cv2.imencode('.jpg', img, encode_param)
-data = pickle.dumps(img, 0)
+data = pickle.dumps(img, 0) # convert image to byte
 size = len(data)
-print("Size of the image : ",size)
-sock.sendall(struct.pack(">L", size) + data)
+print("[!] Size of the image : ",size)
+sock.sendall(struct.pack(">L", size) + data) # send image bytes to server
+
 data = {
     'type' : TYPE_,
     'key' : KEY,
 }
 data = json.dumps(data)
-sock.sendall(data.encode())
+sock.sendall(data.encode()) # send encryption type and key to server
 
 sock.close()
